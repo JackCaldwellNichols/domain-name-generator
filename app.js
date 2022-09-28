a4a5d8218e159dacbe2a4776295f0c907a1ab0a0
jackcaldwellnichols <111219464+JackCaldwellNichols@users.noreply.github.com>
2022-09-28 21:30:26 +0200

    Create app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
 
-let ext = [".com", ".net", ".us", ".io"];
-var randomise = ext[Math.floor(Math.random() * ext.length)];
+var ext = [".com", ".co.uk", ".es"];
+var randomise = ext[Math.floor(Math.random()*ext.length)];
 
-var domainGenerator = (pronoun, adj, noun) => {
-  for (var i = 0; i < pronoun.length; i++) {
-    for (var j = 0; j < adj.length; j++) {
-      for (var r = 0; r < noun.length; r++) {
-        console.log(pronoun[i] + adj[j] + noun[r] + randomise);
-      }
+var domainNameGen = (a, b, c) => {
+    for(let x = 0; x<a.length; x++){
+        for(let y = 0;y<b.length; y++){
+            for(let z=0; z<c.length; z++){
+              console.log(a[x] + b[y] + c[z] + randomise)
+            }
+        }
     }
-  }
-};
-domainGenerator(pronoun, adj, noun);
+}
+
+domainNameGen(pronoun, adj, noun)
